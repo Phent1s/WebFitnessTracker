d59dbf9ea886821bd3b273b70cafe9f82dc27b00
Andrii Ovcharuk <phent1soff@gmail.com>
2025-05-24 20:20:40 +0300

    Initial Commit

---
+++ src/main/java/com/project/webfitnesstracker/WebFitnessTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.project.webfitnesstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFitnessTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFitnessTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/webfitnesstracker/WebFitnessTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.webfitnesstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFitnessTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
