3a677fcde945fae74db5ab9f34c32da3305a1f60
Andrii Ovcharuk <phent1soff@gmail.com>
2025-05-31 01:55:00 +0300

    fix: added functions to repositories

--- src/main/java/com/project/webfitnesstracker/repository/GoalRepository.java
+++ src/main/java/com/project/webfitnesstracker/repository/GoalRepository.java
@@ -4,6 +4,12 @@ import com.project.webfitnesstracker.model.Goal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GoalRepository extends JpaRepository<Goal, Long> {
+    List<Goal> findByOwner_IdOrderByStartDateAsc(Long id);
+
+    List<Goal> findAllByOrderByStartDateAsc();
+
 }
--- src/main/java/com/project/webfitnesstracker/repository/WorkoutRepository.java
+++ src/main/java/com/project/webfitnesstracker/repository/WorkoutRepository.java
@@ -4,6 +4,15 @@ import com.project.webfitnesstracker.model.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WorkoutRepository extends JpaRepository<Workout, Long> {
+
+    List<Workout> findByOwner_IdOrderByDateAsc(Long id);
+
+    List<Workout> findAllByOrderByDateAsc();
+
+
+
 }
