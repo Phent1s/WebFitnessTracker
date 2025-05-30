3aa9a80f1d421f77a4e27ea6485528bef8fbbf5e
Andrii Ovcharuk <phent1soff@gmail.com>
2025-05-31 01:53:28 +0300

    feat: Admin controllers created and finished

---
+++ src/main/java/com/project/webfitnesstracker/controller/admin/AdminWorkoutController.java
@@ -0,0 +1,21 @@
+package com.project.webfitnesstracker.controller.admin;
+
+import com.project.webfitnesstracker.service.WorkoutService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/workouts")
+@RequiredArgsConstructor
+public class AdminWorkoutController {
+    private final WorkoutService workoutService;
+
+    @GetMapping("/all")
+    public String getAllWorkouts(Model model) {
+        model.addAttribute("workouts", workoutService.getAllWorkoutsSortedByDate());
+        return "workouts/workout-list";
+    }
+}
