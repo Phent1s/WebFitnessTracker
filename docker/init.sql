c04f1598173e7c84ecd3ff61db77be5dc6d0ea24
Andrii Ovcharuk <phent1soff@gmail.com>
2025-05-31 22:20:37 +0300

    fix: sequence fixed in DB; removed reset script

--- docker/init.sql
+++ docker/init.sql
@@ -36,27 +36,29 @@ VALUES (1, 'Alex', '$2a$12$dZdcj73oqPdPG5thyyMGYeV7EiiKHjaSJEobwXz1Qa9J15eoVVDaq
        (3, 'John', '$2a$12$YR5ysuNW0NU1LZLdpZeK5.4QjFnMxfZGv6.5hBlYSmw45Vq0Dy1tu', 'USER'); -- Пароль: john123
 
 INSERT INTO workouts (id, user_id, type, date, duration_minutes, calories_burned)
-VALUES (1, 1, 'RUNNING', '2025-06-10', 30, 250),
-       (2, 1, 'SWIMMING', '2025-06-12', 45, 400),
-       (3, 2, 'CYCLING', '2025-06-11', 60, 500),
-       (4, 3, 'YOGA', '2025-06-09', 40, 150);
+VALUES (1, 1, 'Running', '2025-06-10', 30, 250),
+       (2, 1, 'Swimming', '2025-06-12', 45, 400),
+       (3, 2, 'Cycling', '2025-06-11', 60, 500),
+       (4, 3, 'Yoga', '2025-06-09', 40, 150);
 
 INSERT INTO goals (id, user_id, description, target_value, current_value, start_date, end_date, is_achieved)
 VALUES (1, 1, 'Lose 5 kg', 5, 2.5, '2025-01-23', '2025-06-23', FALSE),
        (2, 2, 'Run 100 km', 100, 75, '2025-05-10', '2025-07-10', FALSE),
        (3, 3, 'Burn 5000 calories', 5000, 3200, '2025-05-13', '2025-06-13', FALSE);
 
-create sequence users_id_seq;
+create sequence users_id_seq
+    increment by 50;
 alter sequence users_id_seq owner to admin;
-alter sequence users_id_seq owned by users.id;
 
-create sequence workouts_id_seq;
+
+create sequence workouts_id_seq
+    increment by 50;
 alter sequence workouts_id_seq owner to admin;
-alter sequence workouts_id_seq owned by workouts.id;
 
-create sequence goals_id_seq;
+
+create sequence goals_id_seq
+    increment by 50;
 alter sequence goals_id_seq owner to admin;
-alter sequence goals_id_seq owned by goals.id;
 
 SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));
 SELECT setval('workouts_id_seq', (SELECT MAX(id) FROM workouts));
--- docker/reset-db.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-psql -U $POSTGRES_USER -d $POSTGRES_DB -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
-psql -U $POSTGRES_USER -d $POSTGRES_DB -f /docker-entrypoint-initdb.d/init.sql
